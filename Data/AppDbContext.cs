6fe190229d197430a5720268370b4f576eb6b502
Beacon-creator <adebiyibukola157@gmail.com>
2024-06-07 17:15:45 +0100

    first commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Aspbackend.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aspbackend.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        //User is the model
+        public DbSet<User> Users { get; set; } = default!;
+    }
+}
